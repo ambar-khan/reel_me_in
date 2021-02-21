b899a414997a0a51f33a7ea5a0c01452ae2b3664
Ambar <ambarmogg@me.com>
2021-02-20 16:31:00 -0800

    add initial search through title

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+ <div id='app'>
+  <Search/>
+ </div>
 </template>
+<script>
+import Search from "./components/Search";
+
+export default {
+  name: "App",
+  components: {
+    Search,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+ <div class='search'>
+  <h1>Search</h1>
+  <input type='text' v-model='query' @keyup='getResult(query)'>
+  <div v-for='result in results' :key='result.id'>
+   <p>{{result.title}}</p>
+   <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='100px'>
+  </div>
+ </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "search",
+  data() {
+    return {
+      query: "",
+      results: "",
+    };
+  },
+  methods: {
+    getResult(query) {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie?api_key=a4e8485214a389b8af6f13a3549063d5&query=" +
+            query
+        )
+        .then((response) => {
+          this.results = response.data.results;
+        });
+      console.log(this.results);
+    },
+  },
+};
+</script>
