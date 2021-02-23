3473d1eb46120e2aaa824638be7701f2f36099ff
Ambar <ambarmogg@me.com>
2021-02-22 19:36:43 -0800

    update background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
- <div id='app'>
+ <div id='app' :style="{'background-image':'url(https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80)'}">
   <header>
      <img src="./assets/logo.png" alt="Reel Me In Logo" id="logo">
       <h1>
@@ -38,11 +38,11 @@
                   </br>
                   <input type="number" min="1878" name="year" id="year" v-model='year'>
               </div>
-              <div class="group">
+              <!-- <div class="group">
                   <label for="min-rating">Minimum Rating: </label>
                   </br>
                   <input type="number" min="0" step="0.1" max="10" name="min-rating" id="min-rating" v-model='minrating'>
-              </div>
+              </div> -->
               <div class="group">
                   <label>Viewing Platform:</label>
                   <br>
@@ -69,7 +69,7 @@ import Movie from "./components/Movie";
 export default {
   name: "App",
   components: {
-    Movie
+    Movie,
   },
   data() {
     return {
@@ -90,12 +90,17 @@ export default {
       }
       axios
         .get(
-          "https://api.themoviedb.org/3/discover/movie?api_key=a4e8485214a389b8af6f13a3549063d5&with_cast=&with_genres=" + genre + "&year=" + year + "&vote_average.gte=" + minrating
+          "https://api.themoviedb.org/3/discover/movie?api_key=a4e8485214a389b8af6f13a3549063d5&with_cast=&with_genres=" +
+            genre +
+            "&year=" +
+            year +
+            "&vote_average.gte=" +
+            minrating
         )
         .then((response) => {
           this.results = response.data.results;
         });
-        console.log(genre + " " + year + " " + minrating);
+      console.log(genre + " " + year + " " + minrating);
       console.log(this.results);
     },
   },
@@ -105,56 +110,64 @@ export default {
 
 <style>
 #app {
+  background-image: url(/src/assets/background.jpg);
   text-align: center;
   color: #ffffff;
 }
-    header {
-        background: linear-gradient(0deg, rgba(215,38,56,0) 0%, rgba(215,38,56,1) 100%);
-        padding-bottom: 50px;
-    }
-    #logo {
-        max-width: 100%;
-        width: 300px;
-        margin: 18px auto;
-    }
-    #formpos {
-        margin: auto;
-        padding: 20px;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        width: 70%;
-    }
-    .group{
-        padding: 10px 50px;
-    }
-    .button {
-        padding: 10px 20px;
-        border-radius: 12px;
-        border: none;
-        background-color: #3F88C5;
-        color: #ffffff;
-        box-shadow: 1px 1px 0px #ffffff, 2px 2px 0px #f5c9ce, 3px 3px 0px #eb939c, 4px 4px 0px #e15d6a, 5px 5px 0px #d72638;
-        transition: all 1s;
-        font-size: 1.5rem;
-    }
-    .button:hover, .button:focus {
-        box-shadow: none;
-        transform: translate(5px, 5px);
-        outline: none;
-    }
-    #movie-container {
-      display: flex;
-      justify-content: space-around;
-      align-content: center;
-      padding: 50px;
-      flex-wrap: wrap;
-    }
-    input[type='number'], select {
-      padding: 10px;
-      border-radius: 10px;
-      outline: none;
-      margin: 5px;
-    }
+header {
+  background: linear-gradient(
+    0deg,
+    rgba(215, 38, 56, 0) 0%,
+    rgba(215, 38, 56, 1) 100%
+  );
+  padding-bottom: 50px;
+}
+#logo {
+  max-width: 100%;
+  width: 300px;
+  margin: 18px auto;
+}
+#formpos {
+  margin: auto;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 70%;
+}
+.group {
+  padding: 10px 50px;
+}
+.button {
+  padding: 10px 20px;
+  border-radius: 12px;
+  border: none;
+  background-color: #3f88c5;
+  color: #ffffff;
+  box-shadow: 1px 1px 0px #ffffff, 2px 2px 0px #f5c9ce, 3px 3px 0px #eb939c,
+    4px 4px 0px #e15d6a, 5px 5px 0px #d72638;
+  transition: all 1s;
+  font-size: 1.5rem;
+}
+.button:hover,
+.button:focus {
+  box-shadow: none;
+  transform: translate(5px, 5px);
+  outline: none;
+}
+#movie-container {
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+  padding: 50px;
+  flex-wrap: wrap;
+}
+input[type="number"],
+select {
+  padding: 10px;
+  border-radius: 10px;
+  outline: none;
+  margin: 5px;
+}
 </style>
 
